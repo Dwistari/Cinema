ff131175b4722cea5719f3842b51ccdabe2dcee6
Dwistari <dwistari16@gmail.com>
2025-03-11 23:29:54 +0700

    add listview

---
+++ Cinema/Controllers/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Dwistari on 11/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let movies = ["Inception", "Interstellar", "The Dark Knight", "Avatar", "Titanic"]
+    
+    lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.register(MovieListTableViewCell.self.self, forCellReuseIdentifier: "cell")
+        view.delegate = self
+        view.dataSource = self
+        view.separatorStyle = .none
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavbar()
+        setupView()
+    }
+    
+    
+    private func setupNavbar() {
+        let searchIcon = UIImage(systemName: "magnifyingglass")
+        let tintedSearchIcon = searchIcon?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+        title = "Movies"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: tintedSearchIcon,
+            style: .plain,
+            target: self,
+            action: #selector(search))
+
+    }
+    
+    private func setupView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    
+    @objc private func search() {
+        
+    }
+    
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return movies.count
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieListTableViewCell
+          cell.bindData()
+          return cell
+      }
+      
+      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          print("Selected Movie: \(movies[indexPath.row])")
+          tableView.deselectRow(at: indexPath, animated: true)
+      }
+    
+}
---
+++ Cinema/Views/MovieListTableViewCell.swift
@@ -0,0 +1,189 @@
+//
+//  MovieListTableViewCell.swift
+//  Cinema
+//
+//  Created by Dwistari on 11/03/25.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+    
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.layer.masksToBounds = true
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 8
+        view.layer.borderColor = UIColor.gray.cgColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    
+    lazy var movieImgView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleToFill
+        view.layer.cornerRadius = 4
+        view.backgroundColor = UIColor.lightGray
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var contentContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var movieName: UILabel = {
+        let view = UILabel()
+        view.numberOfLines = 2
+        view.textColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var ratingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 4
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var totalRating: UILabel = {
+        let view = UILabel()
+        view.numberOfLines = 2
+        view.textColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var descLbl: UILabel = {
+        let view = UILabel()
+        view.numberOfLines = 2
+        view.textColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        selectionStyle = .none
+        contentView.backgroundColor = .white
+        contentView.addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            containerView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        containerView.addSubview(movieImgView)
+        NSLayoutConstraint.activate([
+            movieImgView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10),
+            movieImgView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            movieImgView.heightAnchor.constraint(equalToConstant: 120),
+            movieImgView.widthAnchor.constraint(equalToConstant: 100),
+            movieImgView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10),
+        ])
+        
+        containerView.addSubview(contentContainer)
+        NSLayoutConstraint.activate([
+            contentContainer.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+            contentContainer.leadingAnchor.constraint(equalTo: movieImgView.trailingAnchor, constant: 8),
+            contentContainer.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+            contentContainer.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
+        ])
+
+        contentContainer.addSubview(movieName)
+        NSLayoutConstraint.activate([
+            movieName.topAnchor.constraint(equalTo: contentContainer.topAnchor, constant: 12),
+            movieName.leadingAnchor.constraint(equalTo: movieImgView.trailingAnchor, constant: 12),
+            movieName.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12)
+        ])
+        
+        contentContainer.addSubview(ratingStackView)
+        NSLayoutConstraint.activate([
+            ratingStackView.topAnchor.constraint(equalTo: movieName.bottomAnchor, constant: 12),
+            ratingStackView.leadingAnchor.constraint(equalTo: contentContainer.leadingAnchor, constant: 12),
+            ratingStackView.trailingAnchor.constraint(lessThanOrEqualTo: contentContainer.trailingAnchor, constant: -12),
+            ratingStackView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        contentContainer.addSubview(totalRating)
+        NSLayoutConstraint.activate([
+            totalRating.topAnchor.constraint(equalTo: movieName.bottomAnchor, constant: 4),
+            totalRating.leadingAnchor.constraint(equalTo: ratingStackView.trailingAnchor, constant: 12),
+            totalRating.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
+        ])
+        
+        contentContainer.addSubview(descLbl)
+        NSLayoutConstraint.activate([
+            descLbl.topAnchor.constraint(equalTo: ratingStackView.bottomAnchor, constant: 12),
+            descLbl.leadingAnchor.constraint(equalTo: movieImgView.trailingAnchor, constant: 12),
+            descLbl.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
+        ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentContainer.layer.shadowPath = UIBezierPath(roundedRect: contentContainer.bounds, cornerRadius: 8).cgPath
+    }
+    
+    private func setupRatingView(rating: Double) {
+        ratingStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+       
+        let fullStars = Int(rating) // Integer part (e.g., 4 for 4.5)
+        let hasHalfStar = (rating - Double(fullStars)) >= 0.5
+        
+        for _ in 0..<fullStars {
+              let starImageView = UIImageView()
+              starImageView.contentMode = .scaleAspectFit
+              starImageView.tintColor = .systemYellow
+              starImageView.image = UIImage(systemName: "star.fill")
+              ratingStackView.addArrangedSubview(starImageView)
+          }
+        
+        if hasHalfStar {
+            let starImageView = UIImageView()
+            starImageView.contentMode = .scaleAspectFit
+            starImageView.tintColor = .systemYellow
+            starImageView.image = UIImage(systemName: "star.leadinghalf.filled")
+            ratingStackView.addArrangedSubview(starImageView)
+        }
+        
+        let totalStars = 5
+        let remainingStars = totalStars - fullStars - (hasHalfStar ? 1 : 0)
+        for _ in 0..<remainingStars {
+            let starImageView = UIImageView()
+            starImageView.contentMode = .scaleAspectFit
+            starImageView.tintColor = .lightGray
+            starImageView.image = UIImage(systemName: "star")
+            ratingStackView.addArrangedSubview(starImageView)
+        }
+    }
+    
+    func bindData() {
+        movieName.text = "COCO"
+        descLbl.text = "loremdibsuaskdnsddahsdhashdjash"
+        totalRating.text = "8/10"
+        setupRatingView(rating: 4.5)
+    }
+    
+}
